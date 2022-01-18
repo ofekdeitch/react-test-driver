37fa91b83a290cdb0426afead0c8715ca46664e7
Ofek Deitch <ofek.deitch@fiverr.com>
2022-01-18 19:53:45 +0200

    impl

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-export class TestDriver {
-    start() {
-        console.log("Start!");
+import { rest } from "msw";
+import { setupServer, SetupServerApi } from "msw/node";
+import { RouteDefinition } from "./glossary";
+
+export abstract class TestDriver {
+    server?: SetupServerApi;
+
+    async start() {
+        const definitions = this.configureRoutes();
+        const routes = prepareRoutes(definitions);
+
+        this.server = setupServer(...routes);
+        await this.server.listen();
     }
+
+    stop() {
+        this.server?.close();
+    }
+
+    protected abstract configureRoutes(): RouteDefinition<any>[];
+}
+
+function prepareRoutes(definitions: RouteDefinition<any>[]): any[] {
+    return definitions.map((definition) =>
+        rest.get(definition.url, (_req, res, ctx) => {
+            const { data, status } = definition.handler();
+
+            return res(ctx.status(status), ctx.json(data));
+        }),
+    );
 }
