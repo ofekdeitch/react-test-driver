cc84168a58926f0f85fa2726f2506f3bb6cd37eb
Ofek Deitch <62507549+ofekdeitch-fiverr@users.noreply.github.com>
2022-01-18 18:40:53 +0200

    [Setup] Tests (#2)

---
+++ src/spec.ts
@@ -0,0 +1,16 @@
+import * as entry from '.';
+
+
+describe('react-test-driver', () => {
+
+    describe('TestDriver', () => {
+
+        it('should be a class', () => {
+            const typelessEntry = entry as any;
+            const driver = new typelessEntry.TestDriver();
+            expect(driver).not.toBeNull();
+        })
+
+    });
+
+});
